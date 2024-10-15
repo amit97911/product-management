1e6ba0867f966583d932112a1f1b07d80374c53a
Amit Patel <amit97911@gmail.com>
2024-10-16 00:32:05 +0530

    Added soft delete

--- app/Models/LevelZeroCategory.php
+++ app/Models/LevelZeroCategory.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class LevelZeroCategory extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'level_zero_categories';
 
     public function levelOneCategories()
