1663d8d9fb6a653e3d772ecf384c2fb72a83ebb1
Amit Patel <amit97911@gmail.com>
2024-10-15 21:52:53 +0530

    Tradeling API Authentication

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\ProductApiController;
+use App\Models\ApiKey;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(ProductApiController::class)->group(function () {
+    Route::get('create-product', 'createProduct');
 });
